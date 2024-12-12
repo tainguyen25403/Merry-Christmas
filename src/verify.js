96c35217a94f920b2bb86f56b3fe6474ff4e8f28
UmF6ZQ <thanhtaiga49@gmail.com>
2024-12-13 00:05:40 +0700

    commit

---
+++ src/verify.js
@@ -0,0 +1,14 @@
+const correctPassword = "I Love You 3000";
+const submitButton = document.getElementById("submit");
+const passwordInput = document.getElementById("password");
+const errorMessage = document.getElementById("error-message");
+
+submitButton.addEventListener("click", function () {
+  const enteredPassword = passwordInput.value;
+
+  if (enteredPassword === correctPassword) {
+    window.location.href = "src/merry_christmas.html";
+  } else {
+    errorMessage.textContent = "Incorrect password. Please try again.";
+  }
+});
